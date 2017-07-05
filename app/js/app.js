6ee0acac5d1e6a55aa2fefbccfb061c5e9a44b1e
geeekteam <info@geekteam.kz>
2017-07-05 14:33:27 +0600

    pluso added

--- app/js/app.js
+++ app/js/app.js
@@ -368,6 +368,17 @@
         }
     };
 
+    YOURAPPNAME.prototype.socialButtons = function () {
+        if (window.pluso)if (typeof window.pluso.start == "function") return;
+        if (window.ifpluso==undefined) { window.ifpluso = 1;
+            var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+            s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+            s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+            var h=d[g]('body')[0];
+            h.appendChild(s);
+        }
+    };
+
     var app = new YOURAPPNAME(document);
 
     app.appLoad('loading', function () {
@@ -388,5 +399,6 @@
         app.popups();
         app.sliders();
         app.headerImages()
+        app.socialButtons()
     });
 })();
